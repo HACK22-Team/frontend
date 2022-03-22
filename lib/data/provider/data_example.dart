d5fe547d79a04ab3215d76b90ee525f85c82e06b
Amelie <amelie.strupp0@gmail.com>
2022-03-22 19:44:20 +0100

    Added extended setup - including potential state-management solutions and a routing solution

---
+++ lib/data/provider/data_example.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class DataExampleModel extends ChangeNotifier {
+  // Use notifyListeners() when something changes
+  // To put the Provider in the tree use: ChangeNotifierProvider(create: (context) => ..., child: ...)
+  //or MultiProvider()
+  // To consume the provided value: Consumer<DataExampleModel>(
+  // builder: (context, cart, child) {
+  //   return Text(...);
+  // },
+  // DOCUMENTATION: https://pub.dev/packages/provider
+
+}
---
+++ lib/presentation/global_styles/themes/global_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  // Dark Theme
+  static get darkTheme => ThemeData(
+        brightness: Brightness.dark,
+      );
+  // Light Theme
+  static get lightTheme => ThemeData(
+        brightness: Brightness.light,
+      );
+}
