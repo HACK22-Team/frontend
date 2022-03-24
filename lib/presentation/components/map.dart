81ef6b5d030272da14c8e3edb5aa88ed6d887545
Shaurya Sharma <shauryainf.dev1@gmail.com>
2022-03-24 13:14:18 +0100

    feat: add map

--- lib/presentation/components/map.dart
+++ lib/presentation/components/map.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
 class name extends StatelessWidget {
-  const name({Key? key}) : super(key: key);
+  const name({Key? key, required this.size}) : super(key: key);
+  final double size;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Image.asset(
+      'assets/images/logo2.png',
+      scale: size,
+    );
   }
 }
